c87b840b2f0dc18e5c2d0d542ee4dcecd09b5fcf
Bullish Design <BullishDesignLLC@gmail.com>
2025-03-19 15:23:31 -0400

    Update - adding more files to test script lib generation

---
+++ script_library/test/test.py
@@ -0,0 +1,9 @@
+# test Script:
+
+def main():
+  """Main function for test script."""
+  print("Hello, test!")
+
+if __name__ == "__main__":
+  main()
+
--- src/scripts/new_script.py
+++ src/scripts/new_script.py
@@ -7,12 +7,33 @@ from pathlib import Path
 from scripts.config import SCRIPT_DIR
 
 
+def get_template_file_names(dir: str | Path):
+    path_dir = Path(dir)
+    all_files = path_dir.rglob("*")
+    name_list = [f.stem for f in all_files]
+    return name_list
+
+
+def check_name(name: str, avoid_names: list[str]):
+    if name in avoid_names:
+        raise ValueError(f"Name '{name}' already exists in the template directory.")
+        return False
+    return True
+
+
 def main():
     parser = argparse.ArgumentParser(description="Create new uv script with name:")
     parser.add_argument("name", type=str, help="Name of script/library")
     args = parser.parse_args()
+    avoid_names = get_template_file_names(SCRIPT_DIR)
 
     script_name = args.name
+    if not check_name(script_name, avoid_names):
+        print(
+            f"Name '{script_name}' already exists in the template directory. Try a different name."
+        )
+        exit()
+
     script_path = Path(SCRIPT_DIR) / script_name
     print(f"\n\nCreating new script '{script_name}' at location: {script_path}\n\n")
     script_path.mkdir(parents=True, exist_ok=True)
